8efa06900e7184ec52f649b68172d93943c6a1ee
Mikeofic <mikeribeirooficial@gmail.com>
2022-03-15 08:38:45 -0300

    consumindo api

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styled";
 
 export function TransactionTable() {
+    useEffect(() => {
+        api.get('transactions')
+        .then( response => console.log(response.data))
+    }, []);
+
     return (
         <Container>
             <table>
@@ -12,6 +19,18 @@ export function TransactionTable() {
                 </thead>
 
                 <tbody>
+                    <tr>
+                        <td>Desenvolvimento de website</td>
+                        <td className="deposit" >R$ 12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>08/03/2021</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw" >R$ 1.500,00</td>
+                        <td>Casa</td>
+                        <td>01/03/2021</td>
+                    </tr>
                     <tr>
                         <td>Desenvolvimento de website</td>
                         <td className="deposit" >R$ 12.000,00</td>
